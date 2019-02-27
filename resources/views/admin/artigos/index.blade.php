f535094f7518027ef5831f746e3a5ee268ee00e1
unknown <adeloncs@hotmail.com>
2019-02-26 22:23:24 -0300

    initial commit

---
+++ resources/views/admin/artigos/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+  <pagina tamanho="12">
+    <painel titulo="Lista de Artigos">
+      <migalhas v-bind:lista="{{$listaMigalhas}}"></migalhas>
+      <tabela-lista
+      v-bind:titulos="['#','Título','Descrição']"
+      v-bind:itens="[[1,'PHP OO','Curso de PHP OO'],[2,'Vue JS','Curso de Vue JS']]"
+      ordem="desc" ordemcol="1"
+      criar="#criar" detalhe="#detalhe" editar="#editar" deletar="#deletar" token="7887522323"
+
+
+      ></tabela-lista>
+
+    </painel>
+
+  </pagina>
+@endsection
