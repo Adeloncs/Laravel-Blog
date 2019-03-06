8329bbd94609ab2559bec3274d812d277db10c67
unknown <adeloncs@hotmail.com>
2019-03-05 22:08:38 -0300

    tabela update

--- app/Http/Controllers/Admin/ArtigosController.php
+++ app/Http/Controllers/Admin/ArtigosController.php
@@ -16,11 +16,17 @@ class ArtigosController extends Controller
     {
         $listaMigalhas = json_encode([
           ["titulo"=>"Home","url"=>route('home')],
-          ["titulo"=>"Lista de Compras","url"=>""]
+          ["titulo"=>"Lista de artigos","url"=>""]
+        ]);
+
+        $listaArtigos = json_encode([
+          ["id"=>1,"titulo"=>"PHP OO","descricao"=>"Curso de PHP OO"],
+          ["id"=>2,"titulo"=>"Vue JS","descricao"=>"Curso de Vue JS"]
+
         ]);
 
 
-        return view('admin.artigos.index',compact('listaMigalhas'));
+        return view('admin.artigos.index',compact('listaMigalhas','listaArtigos'));
     }
 
     /**
