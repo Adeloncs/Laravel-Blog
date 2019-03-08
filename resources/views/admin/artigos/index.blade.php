49654eec9b5b5e9b789eec9dde834783ac463db8
unknown <adeloncs@hotmail.com>
2019-03-08 15:10:35 -0300

    sistema de editar

--- resources/views/admin/artigos/index.blade.php
+++ resources/views/admin/artigos/index.blade.php
@@ -6,14 +6,13 @@
       <migalhas v-bind:lista="{{$listaMigalhas}}"></migalhas>
 
 
-      <modallink tipo="link" nome="meuModalTeste" titulo="Criar" css=""></modallink>
 
       <tabela-lista
       v-bind:titulos="['#','Título','Descrição']"
       v-bind:itens="{{$listaArtigos}}"
       ordem="desc" ordemcol="1"
       criar="#criar" detalhe="#detalhe" editar="#editar" deletar="#deletar" token="7887522323"
-
+      modal="sim"
 
       ></tabela-lista>
 
@@ -21,7 +20,7 @@
 
   </pagina>
 
-  <modal nome="meuModalTeste">
+  <modal nome="adicionar">
     <painel titulo="Adicionar">
       <formulario css="" action="#" method="put" enctype="multipart/form-data" token="12345">
 
@@ -36,6 +35,21 @@
         <button class="btn btn-info">Adicionar</button>
       </formulario>
     </painel>
+  </modal>
+  <modal nome="editar">
+    <painel titulo="Editar">
+      <formulario css="" action="#" method="put" enctype="multipart/form-data" token="12345">
 
+        <div class="form-group">
+          <label for="titulo">Título</label>
+          <input type="text" class="form-control" id="titulo" name="titulo" v-model="$store.state.item.titulo" placeholder="Título">
+        </div>
+        <div class="form-group">
+          <label for="descricao">Descrição</label>
+          <input type="text" class="form-control" id="descricao" name="descricao" v-model="$store.state.item.descricao" placeholder="Descrição">
+        </div>
+        <button class="btn btn-info">Atualizar</button>
+      </formulario>
+    </painel>
   </modal>
 @endsection
